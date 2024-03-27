76e1c1b0eac5e869fa8e3b0584e62875c283fd6b
natnuo <natnu212@gmail.com>
2024-03-27 18:00:12 -0400

    dynamic port compatibility for heroku

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 import { readFile } from "fs/promises"
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.engine("hbs", engine({ extname: "hbs", defaultLayout: "main", layoutsDir: __dirname + "/views/layouts/" }));
 app.set("views", __dirname + "/views");
@@ -18,73 +18,13 @@ const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     redirectUri: `${HOSTNAME}/currently-playing`,
-    // redirectUri: "natnuo.github.io/spotify-data/callback",
 });
-
-// Create the authorization URL
-// var authorizeURL = spotifyApi.createAuthorizeURL(scope, state);
-
-// https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-// console.log(authorizeURL);
-
-// app.get("/login", (req, res) => {
-//     var state = "sdifujildusiodfu";
-//     var scope = 'user-read-private user-read-email';
-
-//     res.redirect('https://accounts.spotify.com/authorize?' +
-//         querystring.stringify({
-//             response_type: 'code',
-//             client_id: "489bf59cba0a48f7a06ee386920ce727",
-//             scope: scope,
-//             redirect_uri: "http://localhost:3000/callback",
-//             state: state
-//         }
-//     ));
-// });
-
-// const code = "AQB06BPv-zYoIk8VQV_RVvEm1ZJnUplzsUGpB35AE342n-O7rRYRMrWdePY4Psf_3aqLyFDvxxOBKuRguIdf0Px1VuauMKYulG9OrOZUx919BbwqW_6PH6FzsweV0zHp0j9va7Jjd-eRtNIOPCMWDxwsVS2eZYbgig4rsp2z2aCpiDUIZcOcHVbjlumtv9zHTcUy-4eWYcJkCyo73PaPJiK7fCyM3A";
-
-// spotifyApi.authorizationCodeGrant(code).then(
-//     (data) => {
-//         console.log(data.body);
-
-//         spotifyApi.setAccessToken(data.body.access_token);
-//         spotifyApi.setAccessToken(data.body.refresh_token);
-//     },
-//     (err) => {
-//         console.log("Error when retrieving access token", err);
-//     },
-// )
-
-// setInterval(() => {
-
-//     spotifyApi.refreshAccessToken().then(
-//         (data) => {
-//             spotifyApi.setAccessToken(data.body["access_token"]);
-//         },
-//         (err) => {
-//             console.log("Error when refreshing access token", err);
-//         },
-//     )
-// }, 3600*1000-10000)
 
 const generateRandomString = (length=6)=>Math.random().toString(20).substring(2, 2+length)
 const scopes = ["user-read-private", "user-read-email", "user-top-read", "user-read-currently-playing"];
 
 const DISPLAY_WIDTH = 420;
 const DISPLAY_HEIGHT = 88;
-
-// const processSvg = async (name: string, options: { [key: string]: string}) => {
-//     let index = await readFile(__dirname + `/views/${name}.svg`, "utf8");
-    
-//     for (let prop in options) {
-//         if (Object.prototype.hasOwnProperty.call(options, prop)) {
-//             index = index.replace(new RegExp(`{{ *${prop} *}}`, "g"), options[prop]);
-//         }
-//     }
-
-//     return index;
-// }
 
 const CURRENTLY_PLAYING_REDIRECT_URI = `${HOSTNAME}/currently-playing`;
 const TOP_SONGS_REDIRECT_URI = `${HOSTNAME}/top-songs/`;
